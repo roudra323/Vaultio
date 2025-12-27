0e7bc6a69b721e0b41fdccda41a9590c5a02c596
roudra323 <asirroudra@gmail.com>
2025-12-27 21:52:46 +0600

    added localhost + etherscan to the hardhat config

--- Contracts/hardhat.config.ts
+++ Contracts/hardhat.config.ts
@@ -1,8 +1,36 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY = vars.get("PRIVATE_KEY", "");
+const SEPOLIA_RPC_URL = vars.get("SEPOLIA_RPC_URL", "");
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY", "");
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : [],
+      chainId: 11155111,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
 };
 
 export default config;
