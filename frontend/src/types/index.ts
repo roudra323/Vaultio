6189d0996f18605ec00c4e77460a46d9bae41e56
roudra323 <asirroudra@gmail.com>
2025-12-29 16:12:10 +0600

    created separte file for types

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Central types export
+ * Import all types from this file for consistency
+ */
+
+export type { Address, Lock, LockParams, ApproveParams, TransactionStatus } from "./contracts";
+
+export type { DurationOption, WithChildren, AsyncResult } from "./common";
